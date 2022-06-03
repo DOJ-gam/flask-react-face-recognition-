eb7e3c65fa33eab11d0bae3a91014feddb494e50
doj-gam <omarjeng999@gmail.com>
2022-06-03 16:52:08 +0000

    add webcam component and scan

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,15 @@
+import WebCam from "./components/webcam";
+
 function App() {
-  return <div>hello World</div>;
+  return (
+    <div className="app">
+      return{" "}
+      <div className="head">
+        <h2>DOJ's Live Face Recognition App</h2>
+      </div>
+      <WebCam />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/webcam/index.jsx
@@ -0,0 +1,48 @@
+import "./index.scss";
+import React, { useState } from "react";
+import axios from "axios";
+import Webcam from "react-webcam";
+
+const WebCam = () => {
+  const webcamRef = React.useRef(null);
+  const videoContraints = {
+    width: 200,
+    height: 200,
+    facingMode: "user",
+  };
+
+  const [name, setName] = useState("");
+
+  const capture = React.useCallback(async () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+
+    console.log(`ImageSrc = ${imageSrc}`);
+
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api", {
+        data: imageSrc,
+      });
+      console.log(`Response = ${response}`);
+      setName(response.data);
+    } catch (error) {
+      console.log(`Error = ${error}`);
+    }
+  }, [webcamRef]);
+
+  return (
+    <div>
+      <Webcam
+        audio={false}
+        height={300}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={880}
+        videoConstraints={videoContraints}
+      />
+      <button onClick={capture}>Scan</button>
+      <h2>{name}</h2>
+    </div>
+  );
+};
+
+export default WebCam;
